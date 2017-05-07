f25c0ab69bc70cf31e821f175d75e47d981910c7
Adriaan van der Bergh <avanderbergh@gmail.com>
2017-05-07 22:53:25 +0200

    Added Firebase info to the prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC3FfmyAI4BkxSj6Ow7KkJZ_OxwhAuAo40',
+    authDomain: 'brilliant-badger.firebaseapp.com',
+    databaseURL: 'https://brilliant-badger.firebaseio.com',
+    storageBucket: 'brilliant-badger.appspot.com',
+    messagingSenderId: '279180053002'
+  }
 };
